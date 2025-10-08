44751d6ff3e7ee357421523f0d72c0b05b74bfbb
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2025-10-08 15:59:54 +0300

    новые стили

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: 'Описание вашего продукта' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
